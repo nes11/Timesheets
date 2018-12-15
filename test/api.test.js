a01a02d44d596ece7752378d05df40e37ef4d238
nes11 <agnesgaroux47@gmail.com>
2018-12-15 14:59:04 +0000

    timesheet API tests

--- test/api.test.js
+++ test/api.test.js
@@ -2,7 +2,6 @@ const expect = require('chai').expect;
 const axios = require('axios');
 
 const { app } = require('../routes.js');
-const assert = require('assert');
 
 //const timesheet = require('./index.js')
 let server;
@@ -37,10 +36,8 @@ describe('sending a POST req to /createTimesheet', () => {
       description: 'blah, blah'
     };
     return axios.post('http://localhost:4001/createTimesheet', postBody)
-      .then((res) => {
-        expect(res.status).to.equal(200);
-        expect(res.data).to.be.an('object');
-        expect(res.data).to.have.keys('id', 'name', 'time', 'description', 'status');
+      .then(() => {
+        // do some assertions on the createtimesheet response
         return axios.get('http://localhost:4001/')
           .then((res) => {
             res.data.forEach((timesheet) =>{
@@ -50,4 +47,65 @@ describe('sending a POST req to /createTimesheet', () => {
           });
       });
   });
+});
+
+describe('sending a POST req to /markTimesheetComplete/:id', () => {
+  before((done) => {
+    server = app.listen(4001, done);
+  });
+  after(() => {
+    server.close();
+  });
+  const postBody = {
+    name: 'Lesson 3',
+    time: '2018-12-13T18:30:00',
+    description: 'blah, blah'
+  };
+
+  it('should change the status of the ID`d timesheet to completed', () => {
+    return axios.post('http://localhost:4001/createTimesheet', postBody)
+      .then(res => {
+        const password = {
+          password: 'ham sandwich'
+        };
+        return axios.post(`http://localhost:4001/markTimesheetComplete/${res.data.id}`, password)
+          .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.data).to.be.an('object');
+            expect(res.data).to.have.keys('id', 'name', 'time', 'description', 'status');
+            expect(res.data.status).to.equal('completed');
+          });
+      });
+  });
+
+  it('should return an error400 and a still active timesheet when given wrong ID', () => {
+    return axios.post('http://localhost:4001/createTimesheet', postBody)
+      .then((res1) => {
+        const password = {
+          password: 'ham sandwich'
+        };
+        return axios.post('http://localhost:4001/markTimesheetComplete/invalidId', password, { validateStatus: false })
+          .then((res2) => {
+            expect(res2.status).to.equal(400);
+            expect(res2.data.error).to.equal('timesheet not found for id invalidId');
+            expect(res1.data.status).to.equal('active');
+          });
+      });
+  });
+
+  it('should return an error401 and a still active timesheet when given wrong password', () => {
+    return axios.post('http://localhost:4001/createTimesheet', postBody)
+      .then(res1 => {
+        const password = {
+          password: 'cheese sandwich'
+        };
+        return axios.post(`http://localhost:4001/markTimesheetComplete/${res1.data.id}`, password, { validateStatus: false })
+          .then((res2) => {
+            expect(res2.status).to.equal(401);
+            expect(res2.data.error).to.equal('incorrect password');
+            expect(res1.data.status).to.equal('active');
+          });
+      });
+  });
+
 });
