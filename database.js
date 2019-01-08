6c6fdf0a109a2fae71d172c3464121447156270c
nes11 <agnesgaroux47@gmail.com>
2019-01-08 22:43:46 +0000

    change the database name and allowed mongo auth to come from env vars

--- database.js
+++ database.js
@@ -1,12 +1,26 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const localUrl = 'mongodb://localhost:27017';
+const prodUrl = 'mongodb://mongo:27017';
+
+const dbName = 'agnes-timesheets';
+const timesheetCollection = 'timesheets';
+
+const getConnection = async () => {
+  const user = process.env['MONGO_USERNAME'];
+  const password = process.env['MONGO_PASSWORD'];
+  if (!user || !password) {
+    return MongoClient.connect(localUrl);
+  } else {
+    return MongoClient.connect(prodUrl, { auth: { user, password } });
+  }
+};
 
 const saveTimesheet = async (timesheet) => {
   try {
-    const client = await MongoClient.connect(url);
+    const client = await getConnection();
     await client
-      .db('myTimesheetDatabase')
-      .collection('myTimesheetCollection')
+      .db(dbName)
+      .collection(timesheetCollection)
       .insertOne(timesheet);
     client.close();
   } catch(err) {
@@ -16,9 +30,9 @@ const saveTimesheet = async (timesheet) => {
 
 const readTimesheets = async () => {
   try {
-    const client = await MongoClient.connect(url);
-    const db = client.db('myTimesheetDatabase');
-    const myTimesheetCollection = db.collection('myTimesheetCollection');
+    const client = await getConnection();
+    const db = client.db(dbName);
+    const myTimesheetCollection = db.collection(timesheetCollection);
     const result = await myTimesheetCollection.find({}).project({_id:0}).toArray();
     client.close();
     return result;
@@ -29,9 +43,9 @@ const readTimesheets = async () => {
 
 const completeTimesheetById = async (timesheetId) => {
   try {
-    const client = await MongoClient.connect(url);
-    const db = client.db('myTimesheetDatabase');
-    const myTimesheetCollection = db.collection('myTimesheetCollection');
+    const client = await getConnection();
+    const db = client.db(dbName);
+    const myTimesheetCollection = db.collection(timesheetCollection);
     const result = await myTimesheetCollection.findOneAndUpdate(
       { id: timesheetId },
       { $set: { status: 'completed' } },
@@ -46,9 +60,9 @@ const completeTimesheetById = async (timesheetId) => {
 
 const deleteTimesheetById = async (timesheetId) => {
   try {
-    const client = await MongoClient.connect(url);
-    const db = client.db('myTimesheetDatabase');
-    const myTimesheetCollection = db.collection('myTimesheetCollection');
+    const client = await getConnection();
+    const db = client.db(dbName);
+    const myTimesheetCollection = db.collection(timesheetCollection);
     await myTimesheetCollection.deleteOne({ id: timesheetId });
     client.close();
   } catch(err) {
