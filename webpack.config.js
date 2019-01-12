d5c86e24ba7f1e9e26c3f9253437c5f6553ec138
nes11 <agnesgaroux47@gmail.com>
2019-01-12 14:37:56 +0000

    added polyfill to use async func in front end code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: './ui/index.jsx',
+  entry: ['@babel/polyfill', './ui/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'assets'),
     filename: 'bundle.js',
